494e89dd225efaeae0362cb57233bbc21ce3fc97
Jesse Johns <jesse.m.johns@gmail.com>
2014-08-25 21:53:32 -0500

    Updated _ext_det.m to handle time in Serpent 2 output.

--- +mss/Serp_ext_det.m
+++ +mss/Serp_ext_det.m
@@ -1,8 +1,6 @@
 function res = Serp_ext_det(file)
 %%
 %
-%   Compatable with Serpent 2 (maybe Serpent 1??)
-%
 %   This function extracts detector data from a detector file.  This
 %   attempts to be general enough to build information from the near
 %   limitless possibilities of detectors.include "detectors"
@@ -10,7 +8,12 @@ function res = Serp_ext_det(file)
 %   res.flag - indicates the type of detector:
 %       1    - Single value.
 %       2    - f(E)
-%       3    - f(r,E)
+%       3    - f(r,E), or f(r,E,t)
+%
+%   res.value - Value for each detector whose output varies:
+%   res.err   - Relative error of the detector
+%   res.arrar - Coordinates
+%
 %
 %% Script:
 
@@ -40,23 +43,35 @@ while done == 0 % Loop until all detectors are read
         R = eval(det_name);
         
         % Clear flags:
+        cor = 0;
         mesh = 0;
         dim = 0;
         eng = 0;
+        time = 0;
         dx = 0; dy = 0; dz = 0;
+        
+        if size(R,2) > 12
+            time = 1;
+            cor = 1;
+        end
         
         N = length(R(:,1));
         
         if N > 1
-            
+
             % Determine detector type:
-            if max(R(:,2)) > 1
+            if max(R(:,2+cor)) > 1
                 eng = 1;
                 E = eval([det_name 'E']);
                 NE = length(E);
             end
             
-            if max(R(:,10)) > 1
+            if time > 0
+                T = eval([det_name 'T']);
+                NT = length(T);
+            end
+            
+            if max(R(:,10+cor)) > 1
                 mesh = 1;
                 dx = 1;
                 dim = dim + 1;
@@ -64,14 +79,15 @@ while done == 0 % Loop until all detectors are read
                 NX{dim} = length(X{dim});
             end
             
-            if max(R(:,9)) > 1
+            if max(R(:,9+cor)) > 1
                 mesh = 1;
                 dy = 1;
                 dim = dim + 1;
                 X{dim} = eval([det_name 'Y']);
                 NX{dim} = length(X{dim});
             end
-            if max(R(:,8)) > 1
+            
+            if max(R(:,8+cor)) > 1
                 mesh = 1;
                 dz = 1;
                 dim = dim + 1;
@@ -79,49 +95,95 @@ while done == 0 % Loop until all detectors are read
                 NX{dim} = length(X{dim});
             end
             
+            
             % Fill result output:
             if mesh == 1
                 
                 % Here, the reshape command is used and the array matrix
                 % formulated to match the way reshape organizes the
                 % results.
-                if eng == 1 && dim == 3
-                    res.value{idx,1} = reshape(R(:,11),NX{1},NX{2},NX{3},NE);
-                    res.err{idx,1} = reshape(R(:,12),NX{1},NX{2},NX{3},NE);
+                if eng == 1 && dim == 3 && time == 1
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NX{3},NE,NT);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NX{3},NE,NT);
+                    [a b c d e] = ndgrid(X{1}(:,3),X{2}(:,3),X{3}(:,3),E(:,3),T(:,3));
+                    res.array{idx,1} = a;
+                    res.array{idx,2} = b;
+                    res.array{idx,3} = c;
+                    res.array{idx,4} = d;
+                    res.array{idx,5} = e;
+                elseif eng == 1 && dim == 2 && time == 1
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NE,NT);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NE,NT);
+                    [a b c d] = ndgrid(X{1}(:,3),X{2}(:,3),E(:,3),T(:,3));
+                    res.array{idx,1} = a;
+                    res.array{idx,2} = b;
+                    res.array{idx,3} = c;
+                    res.array{idx,4} = d;
+                 elseif eng == 1 && dim == 1 && time == 1
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NE,NT);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NE,NT);
+                    [a b c] = ndgrid(X{1}(:,3),E(:,3),T(:,3));
+                    res.array{idx,1} = a;
+                    res.array{idx,2} = b;
+                    res.array{idx,3} = c;
+                elseif eng == 1 && dim == 3
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NX{3},NE);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NX{3},NE);
                     [a b c d] = ndgrid(X{1}(:,3),X{2}(:,3),X{3}(:,3),E(:,3));
                     res.array{idx,1} = a;
                     res.array{idx,2} = b;
                     res.array{idx,3} = c;
                     res.array{idx,4} = d;
                 elseif eng == 1 && dim == 2
-                    res.value{idx,1} = reshape(R(:,11),NX{1},NX{2},NE);
-                    res.err{idx,1} = reshape(R(:,12),NX{1},NX{2},NE);
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NE);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NE);
                     [a b d] = ndgrid(X{1}(:,3),X{2}(:,3),X{3}(:,3),E(:,3));
                     res.array{idx,1} = a;
                     res.array{idx,2} = b;
                     res.array{idx,4} = d;
                 elseif eng == 1 && dim == 1
-                    res.value{idx,1} = reshape(R(:,11),NX{1},NE);
-                    res.err{idx,1} = reshape(R(:,12),NX{1},NE);
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NE);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NE);
                     [a d] = ndgrid(X{1}(:,3),E(:,3));
                     res.array{idx,1} = a;
                     res.array{idx,4} = d;
+                elseif time == 1 && dim == 3
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NX{3},NT);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NX{3},NT);
+                    [a b c d] = ndgrid(X{1}(:,3),X{2}(:,3),X{3}(:,3),T(:,3));
+                    res.array{idx,1} = a;
+                    res.array{idx,2} = b;
+                    res.array{idx,3} = c;
+                    res.array{idx,4} = d;
+                elseif time == 1 && dim == 2
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NT);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NT);
+                    [a b d] = ndgrid(X{1}(:,3),X{2}(:,3),X{3}(:,3),T(:,3));
+                    res.array{idx,1} = a;
+                    res.array{idx,2} = b;
+                    res.array{idx,4} = d;
+                elseif time == 1 && dim == 1
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NT);
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NT);
+                    [a d] = ndgrid(X{1}(:,3),T(:,3));
+                    res.array{idx,1} = a;
+                    res.array{idx,4} = d;
                 elseif eng == 0 && dim == 3
-                    res.value{idx,1} = reshape(R(:,11),NX{1},NX{2},NX{3});
-                    res.err{idx,1} = reshape(R(:,12),NX{1},NX{2},NX{3});
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2},NX{3});
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2},NX{3});
                     [a b c] = ndgrid(X{1}(:,3),X{2}(:,3),X{3}(:,3));
                     res.array{idx,1} = a;
                     res.array{idx,2} = b;
                     res.array{idx,3} = c;
                 elseif eng == 0 && dim == 2
-                    res.value{idx,1} = reshape(R(:,11),NX{1},NX{2});
-                    res.err{idx,1} = reshape(R(:,12),NX{1},NX{2});
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1},NX{2});
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1},NX{2});
                     [a b] = ndgrid(X{1}(:,3),X{2}(:,3));
                     res.array{idx,1} = a;
                     res.array{idx,2} = b;
                 else
-                    res.value{idx,1} = reshape(R(:,11),NX{1});
-                    res.err{idx,1} = reshape(R(:,12),NX{1});
+                    res.value{idx,1} = reshape(R(:,11+cor),NX{1});
+                    res.err{idx,1} = reshape(R(:,12+cor),NX{1});
                     [a] = ndgrid(X{1}(:,3));
                     res.array{idx,1} = a;
                 end
@@ -130,10 +192,15 @@ while done == 0 % Loop until all detectors are read
                 
             elseif eng == 1
                 res.flag{idx} = 2;
-                
-                res.value{idx,1} = R(:,11);
-                res.err{idx,1} = R(:,12);
+                res.value{idx,1} = R(:,11+cor);
+                res.err{idx,1} = R(:,12+cor);
                 res.array{idx,1} = E(:,3);
+            elseif eng == 1 && time == 1
+                res.value{idx,1} = reshape(R(:,11+cor),NE,NT);
+                res.err{idx,1} = reshape(R(:,12+cor),NE,NT);
+                [a b] = ndgrid(E(:,3),T(:,3));
+                res.array{idx,1} = a;
+                res.array{idx,2} = b;
             end
             
         else
